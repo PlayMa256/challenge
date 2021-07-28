f564005dc423db6d50ec3fdc6eda7be13e24d05b
PlayMa256 <matheus.g.silva@hotmail.com>
2021-07-28 15:56:18 -0300

    add seeds

---
+++ seeds/default/1627498169863_initial_seed.sql
@@ -0,0 +1,3 @@
+insert into public.long_tails (tail, json_id) VALUES ('best-hello-ever', 1);
+insert into public.long_tails (tail, json_id) VALUES ('best-hello-world-ever', 2);
+insert into public.long_tails (tail, json_id) VALUES ('best-world-ever', 3);
