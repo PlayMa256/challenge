4d2d167e08c9a4227251a71295ec81c6316021fb
PlayMa256 <matheus.g.silva@hotmail.com>
2021-07-28 17:07:23 -0300

    fix: envs

--- config.ts
+++ config.ts
@@ -6,11 +6,6 @@ import {
 
 const httpLink = new HttpLink({
   uri: process.env.NEXT_PUBLIC_HASURA_ENDPOINT,
-  ...(process.env.NEXT_PUBLIC_HASURA_ENDPOINT) ? {
-    headers: {
-      "x-hasura-admin-secret": process.env.NEXT_PUBLIC_HASURA_ENDPOINT
-    }
-  } : {}
 });
 
 export const client = new ApolloClient({
